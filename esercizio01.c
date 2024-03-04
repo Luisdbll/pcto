2cbcd1b06762162c28c43b9c80e906eb57134814
Luisdbll <162116653+Luisdbll@users.noreply.github.com>
2024-03-04 16:01:39 +0000

    cl

---
+++ esercizio01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+ {
+    int numero;
+    numero=10;
+    printf("%d",numero);
+ }
---
+++ esercizio02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char lettera;
+    lettera='l';
+    printf("%c\n",lettera);
+}
